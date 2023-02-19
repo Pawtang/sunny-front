d3f43de21530f8829e4d8741cc4ec120800c5b34
Ben Dibuz <ben.dibuz@gmail.com>
2023-02-19 14:58:46 -0600

    Begin building elements

--- Sunny/src/components/Day.tsx
+++ Sunny/src/components/Day.tsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { useState } from "react";
 import Rating from "../elements/Rating";
 
-const Day = () => {
-  const [dailyRating, setDailyRating] = useState(0);
+const Day: FunctionComponent = () => {
+  // const [dailyRating, setDailyRating] = useState(0);
 
   return (
     <div className="Day border-2">
       <div className="container mx-auto p-4 mt-4 border-2">
-        <h1 className="text-3xl font-bold underline">Hello Name</h1>
-        <h2>Today is Month Day, Year</h2>
-        <h2>How was your day today?</h2>
-        <h2>{dailyRating}</h2>
+        <div className="mx-auto">
+          <h1 className="text-3xl font-bold underline">Hello Name</h1>
+          <h2>Today is Month Day, Year</h2>
+          <h2>How was your day today?</h2>
+        </div>
+        {/* <h2>{dailyRating}</h2> */}
       </div>
-
-      <Rating type="range" value="quality"></Rating>
-      <Rating type="bool" value="exercise"></Rating>
-      <Rating type="num" value="sleep"></Rating>
+      <div className="container mx-auto mt-4 max-w-md border-2">
+        <Rating type="range" value="Quality"></Rating>
+        <Rating type="bool" value="Exercise"></Rating>
+        <Rating type="num" value="Sleep"></Rating>
+      </div>
     </div>
   );
 };
--- Sunny/src/elements/Rating.tsx
+++ Sunny/src/elements/Rating.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { useState } from "react";
 import { Interface } from "readline";
 
-const evaluateRating = (value: string, type: string, rating: number) => {
-  if ((value = "quality")) {
+const evaluateRating = (value: string, type: string, rating?: number) => {
+  if (type === "range" && value === "Quality") {
     switch (rating) {
       case 0:
         return "Miserable day...";
@@ -18,6 +18,15 @@ const evaluateRating = (value: string, type: string, rating: number) => {
       default:
         return "Rate your day!";
     }
+  } else if (type === "num") {
+    if (value === "Sleep") {
+      return `${rating} hours`;
+    }
+    return value;
+  } else if (type === "bool") {
+    if (rating === 0) {
+      return "Yes";
+    } else return "No";
   }
 };
 
@@ -26,25 +35,52 @@ interface ratingProps {
   value: string;
 }
 
-const Rating = (props: object) => {
+const Rating: FunctionComponent<ratingProps> = (props) => {
   const { type, value } = props;
   const [rating, setRating] = useState(0);
+
   return (
-    <div className="container dayrating mx-auto p-4">
+    <div className="container dayrating mx-auto p-6">
+      <h2>{rating}</h2>
       <div className="grid grid-cols-3 mx-auto border-2">
-        <div className="border-1 mx-auto">Overall</div>
+        <div className="border-1 mx-auto">{value}</div>
         <div className="border-1 mx-auto">
-          <input
-            type="range"
-            name="day-quality"
-            id="day-quality"
-            min={0}
-            max={4}
-            value={rating}
-            onChange={(e) => {
-              setRating(e.target.valueAsNumber);
-            }}
-          />
+          {type === "range" && (
+            <input
+              type="range"
+              name="day-quality"
+              id="day-quality"
+              min={0}
+              max={4}
+              value={rating}
+              onChange={(e) => {
+                setRating(e.target.valueAsNumber);
+              }}
+            />
+          )}
+          {type === "bool" && (
+            <input
+              type="checkbox"
+              name="inputBool"
+              id="inputBool"
+              value={rating}
+              onChange={(e) => {
+                setRating(e.target.valueAsNumber);
+              }}
+            />
+          )}
+          {type === "num" && (
+            <input
+              type="number"
+              name="inputNum"
+              id="inputNum"
+              value={rating}
+              onChange={(e) => {
+                setRating(e.target.valueAsNumber);
+              }}
+              className="border-1"
+            />
+          )}
         </div>
         <div className="border-1 mx-auto">
           {evaluateRating(value, type, rating)}
