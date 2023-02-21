0249f941becd76087f2bd9811c185df5a5fbe3ad
Ben Dibuz <ben.dibuz@gmail.com>
2023-02-20 23:45:19 -0600

    playing with css

--- Sunny/src/components/Day.tsx
+++ Sunny/src/components/Day.tsx
@@ -10,25 +10,33 @@ const Day: FunctionComponent = () => {
   // const [dailyRating, setDailyRating] = useState(0);
 
   return (
-    <div className="Day">
-      <div className="container mx-auto p-4 mt-4">
-        <div className="mx-auto">
-          <h1 className="text-3xl font-bold underline center">Hello, Name</h1>
-          <h2 className="center">
-            Today is <i>{today.format("MMMM DD, YYYY")}</i>
-          </h2>
-          <h2 className="center">
-            <b> How was your day today?</b>
-          </h2>
+    <div className="Day h-screen">
+      <div className="container h-96 spacer"></div>
+      <div className="journal max-w-lg mx-auto">
+        <div className="container mx-auto p-4 mt-4 ">
+          <div className="mx-auto ">
+            <h1 className="text-3xl font-bold underline center ">
+              Hello, Name
+            </h1>
+            <h2 className="center ">
+              Today is <i>{today.format("MMMM DD, YYYY")}</i>
+            </h2>
+            <h2 className="center">
+              <b> How was your day today?</b>
+            </h2>
+          </div>
+          {/* <h2>{dailyRating}</h2> */}
         </div>
-        {/* <h2>{dailyRating}</h2> */}
+        <div className="rating-inputs container mx-auto mt-4 max-w-lg">
+          <RangeRating value="Quality"></RangeRating>
+          <BooleanRating value="Exercise"></BooleanRating>
+          <NumberRating value="Sleep"></NumberRating>
+          <NumberRating value="Drinks"></NumberRating>
+          <NumberRating value="Miles Run"></NumberRating>
+        </div>
       </div>
-      <div className="rating-inputs container mx-auto mt-4 max-w-md">
-        <RangeRating value="Quality"></RangeRating>
-        <BooleanRating value="Exercise"></BooleanRating>
-        <NumberRating value="Sleep"></NumberRating>
-        <NumberRating value="Drinks"></NumberRating>
-        <NumberRating value="Miles Run"></NumberRating>
+      <div className="container submit mx-auto flex flex-col items-center m-10">
+        <button className=" btn-submit">Submit</button>
       </div>
     </div>
   );
