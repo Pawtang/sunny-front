517c3eff62a4467bde3d92a6f4267ee3b8ce23d4
Ben Dibuz <ben.dibuz@gmail.com>
2023-05-29 12:36:32 -0500

    conditional grid width

--- Sunny/src/components/Month.tsx
+++ Sunny/src/components/Month.tsx
@@ -15,9 +15,9 @@ import Modal from "./Modal";
 const Month: FunctionComponent = () => {
   const today = dayjs();
   const monthCount = today.daysInMonth();
-  const containerStyle = () => {
-    return "grid-cols-7";
-  };
+  // const containerStyle = () => {
+  //   return "grid-cols-7";
+  // };
 
   const [month, setMonth] = useState<IDay[] | undefined>([]);
   const [selectedMonth, setSelectedMonth] = useState("");
@@ -53,63 +53,63 @@ const Month: FunctionComponent = () => {
           buttonText="📅"
           styleTags="z-50"
         ></ActionButton>
-      </div>
 
-      <div className="mx-4 inline">
-        <input
-          type="range"
-          value={time}
-          min={0}
-          max={23}
-          onChange={(e) => {
-            setTime(e.target.valueAsNumber);
-          }}
-        />
-      </div>
-
-      <Modal
-        id="modalContainer"
-        onClick={() => {
-          setModalVisibility(!modalVisibility);
-        }}
-        visible={modalVisibility}
-        content={
-          <MonthPicker
-            closeModal={() => {
-              setModalVisibility(!modalVisibility);
+        <div className="mx-4 inline">
+          <input
+            type="range"
+            value={time}
+            min={0}
+            max={23}
+            onChange={(e) => {
+              setTime(e.target.valueAsNumber);
             }}
-            selectedMonth={selectedMonth}
-            selectedYear={selectedYear}
-            setMonth={setSelectedMonth}
-            setYear={setSelectedYear}
-          ></MonthPicker>
-        }
-      ></Modal>
-
-      <div className="container justify-content mx-auto mt-4 h-100">
-        <div className="container justify-content mx-auto max-w-lg">
-          <h1 className="text-2xl mx-auto text-center">
-            <b>Today is {today.format("MMMM DD, YYYY")}</b>
-          </h1>
-          <h1 className="text-xl text-center">
-            {`There are ${monthCount} days in ${today.format("MMMM")}.`}
-            <br />
-            One box represents each day:
-          </h1>
+          />
         </div>
-        <div
-          className={`mx-auto shadow-lg mt-10 container grid place-content-center rounded bg-white/50 p-4 max-w-2xl ${containerStyle()}`}
-        >
-          {month &&
-            month.map((day) => (
-              <div id={day.id.toString()} key={day.id}>
-                <CalendarDay
-                  dayIndex={day.id}
-                  dayRating={day.dayRating}
-                  notes={day.notes}
-                ></CalendarDay>
-              </div>
-            ))}
+
+        <Modal
+          id="modalContainer"
+          onClick={() => {
+            setModalVisibility(!modalVisibility);
+          }}
+          visible={modalVisibility}
+          content={
+            <MonthPicker
+              closeModal={() => {
+                setModalVisibility(!modalVisibility);
+              }}
+              selectedMonth={selectedMonth}
+              selectedYear={selectedYear}
+              setMonth={setSelectedMonth}
+              setYear={setSelectedYear}
+            ></MonthPicker>
+          }
+        ></Modal>
+
+        <div className="container justify-content mx-auto mt-4 sm:h-100 h-full">
+          <div className="container justify-content mx-auto max-w-lg">
+            <h1 className="text-2xl mx-auto text-center">
+              <b>Today is {today.format("MMMM DD, YYYY")}</b>
+            </h1>
+            <h1 className="text-xl text-center">
+              {`There are ${monthCount} days in ${today.format("MMMM")}.`}
+              <br />
+              One box represents each day:
+            </h1>
+          </div>
+          <div
+            className={`mx-auto shadow-lg mt-10 container grid grid-cols-3 sm:grid-cols-7 place-content-center rounded bg-white/50 p-4 max-w-2xl `}
+          >
+            {month &&
+              month.map((day) => (
+                <div id={day.id.toString()} key={day.id}>
+                  <CalendarDay
+                    dayIndex={day.id}
+                    dayRating={day.dayRating}
+                    notes={day.notes}
+                  ></CalendarDay>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </div>
