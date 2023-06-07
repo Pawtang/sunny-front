8c2efcde6a1ac2ddb3cab9f829e719a560965d0d
Ben Dibuz <ben.dibuz@gmail.com>
2023-06-06 23:38:43 -0500

    working pearson

--- Sunny/src/utilities/Correlation.tsx
+++ Sunny/src/utilities/Correlation.tsx
@@ -21,15 +21,34 @@ export const Correlation = (days: Array<dayObject>) => {
         [];
   });
 
-
-const pearsonCorrelation = (x: Array<number>, y: Array<number>)
+  const pearsonCorrelation = (x: Array<number>, y: Array<number>) => {
+    // if (x.length !== y.length) throw new Error("unequal lengths");
+    if (x.length !== y.length) return "must be equal lengths!";
+    const xSum = x.reduce((sum, value) => sum + value);
+    const ySum = y.reduce((sum, value) => sum + value);
+    const xMean = xSum / x.length;
+    const yMean = ySum / y.length;
+    let A = 0;
+    let B = 0;
+    let C = 0;
+    for (let i = 0; i < x.length; i++) {
+      const xdiff = x[i] - xMean;
+      const ydiff = y[i] - yMean;
+      A += xdiff * ydiff;
+      B += xdiff ** 2;
+      C += ydiff ** 2;
+    }
+    return A / Math.sqrt(B * C);
+  };
 
   useEffect(() => {
     console.log("days,", days);
     console.log(dayRatingArray);
     console.log(attributeArray);
-    // writeArrays();
-    // writeArrays();
+    console.log(
+      "pearson",
+      pearsonCorrelation([2, 3, 1, 0, 5, 5, 5, 5], [1, 7, 7, 4, 5, 5, 5, 6])
+    );
   }, [dayRatingArray, attributeArray]);
 
   // console.log(days);
