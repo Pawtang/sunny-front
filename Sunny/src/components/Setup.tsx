523221539580c9e5ed06f23b1e45a9cab75252d0
Pawtang <ben.dibuz@gmail.com>
2023-07-09 08:53:56 -0700

    Update layout

--- Sunny/src/components/Setup.tsx
+++ Sunny/src/components/Setup.tsx
@@ -42,32 +42,19 @@ const Setup = () => {
   useEffect(() => {
     APIGetAuthy(SETUP_URL, (data: any) => {
       setAttributes(
-        data.filter(
-          (attr: attributeObject) =>
-            ![
-              HOURS_SLEPT,
-              MILES_RUN,
-              MINUTES_READ,
-              WENT_TO_GYM,
-              DRINKS_HAD,
-            ].includes(attr.name)
-        )
+        data
+        // .filter(
+        //   (attr: attributeObject) =>
+        //     ![
+        //       HOURS_SLEPT,
+        //       MILES_RUN,
+        //       MINUTES_READ,
+        //       WENT_TO_GYM,
+        //       DRINKS_HAD,
+        //     ].includes(attr.name)
+        // )
       );
     });
-    // getAttributesForUser(dummyUserID, (data: any) => {
-    //   setAttributes(
-    //     data.filter(
-    //       (attr: attributeObject) =>
-    //         ![
-    //           HOURS_SLEPT,
-    //           MILES_RUN,
-    //           MINUTES_READ,
-    //           WENT_TO_GYM,
-    //           DRINKS_HAD,
-    //         ].includes(attr.name)
-    //     )
-    //   );
-    // });
   }, []);
 
   const assignType = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -106,123 +93,144 @@ const Setup = () => {
       <div
         className={`bg-gradient-to-r from-cyan-500 to-blue-500 min-h-screen flex items-center justify-center`}
       >
-        <form className="bg-white/50 hover:bg-white/60 transition-all p-6 rounded-lg shadow-md max-w-md hover:-translate-y-1 duration-200 ease-linear">
-          <h2 className="text-3xl font-bold mb-6 text-center">
-            Set Up Your Tracking
-          </h2>
+        <form className="my-24 container-2xl bg-white/50 hover:bg-white/60 transition-all p-6 rounded-lg shadow-md hover:-translate-y-1 duration-200 ease-linear">
+          <div className="mb-6">
+            <h2 className="text-3xl font-bold  text-center">
+              Set Up Your Tracking
+            </h2>
+            <i>
+              You can change these later, but it's best to set up correctly the
+              first time to collect good data!{" "}
+            </i>
+          </div>
           <h2 className="text-xl ">
-            <b>You Are Tracking:</b>
+            <b>Currently Tracking:</b>
+          </h2>
+
+          <div className="outline outline-1 outline-white py-1 px-2 my-2 rounded-md grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
             {attributes.map((attribute) => {
               return (
-                <ul className="text-lg">
-                  <li>
-                    <b>{`${attribute.name}:`}</b> {`${attribute.type}`}
-                  </li>
-                </ul>
+                <div className="my-4 w-100">
+                  {/* <ul className="text-lg">
+                  <li className="my-8"> */}
+                  <div className="p-2 bg-white/50 rounded-md inline w-100 mx-2">
+                    <img
+                      className="w-6 inline"
+                      src={`/icons/${
+                        attribute.type === "boolean"
+                          ? "true-false.png"
+                          : "numbers.png"
+                      }`}
+                      alt=""
+                    />
+                  </div>
+                  <b>{`${attribute.name}`}</b>
+                  {/* </li>
+                </ul> */}
+                </div>
               );
             })}
+          </div>
+          <h2 className="text-xl mt-4">
+            <b>Add Attributes:</b>
           </h2>
 
           {/* Common Attributes */}
-          <div className="my-4">
-            <h2 className="text-xl ">
-              <b>Common Attributes</b>
-            </h2>
-            <CheckItem
-              // name="sleep"
-              // id="check-sleep"
-              label="Hours Slept"
-              checked={hoursSlept}
-              onClick={() => setHoursSlept(!hoursSlept)}
-            ></CheckItem>
-            <CheckItem
-              // name="miles-run"
-              // id="check-miles-run"
-              label="Miles Run"
-              checked={milesRun}
-              onClick={() => setMilesRun(!milesRun)}
-            ></CheckItem>
+          <div className="outline outline-1 outline-white/50 py-1 px-2 my-2 rounded-md">
+            <div className="my-4 ">
+              <h2 className="text-xl ">
+                <b>Common Attributes</b>
+              </h2>
+              <CheckItem
+                label="Hours Slept"
+                checked={hoursSlept}
+                onClick={() => setHoursSlept(!hoursSlept)}
+              ></CheckItem>
+              <CheckItem
+                label="Miles Run"
+                checked={milesRun}
+                onClick={() => setMilesRun(!milesRun)}
+              ></CheckItem>
 
-            <CheckItem
-              // name="minutes-read"
-              // id="check-minutes-read"
-              label="Minutes Read"
-              checked={minutesRead}
-              onClick={() => setMinutesRead(!minutesRead)}
-            ></CheckItem>
+              <CheckItem
+                label="Minutes Read"
+                checked={minutesRead}
+                onClick={() => setMinutesRead(!minutesRead)}
+              ></CheckItem>
 
-            <CheckItem
-              // name="gym"
-              // id="check-gym"
-              label="Went to Gym"
-              checked={wentToGym}
-              onClick={() => setWentToGym(!wentToGym)}
-            ></CheckItem>
+              <CheckItem
+                // name="gym"
+                // id="check-gym"
+                label="Went to Gym"
+                checked={wentToGym}
+                onClick={() => setWentToGym(!wentToGym)}
+              ></CheckItem>
 
-            <CheckItem
-              // name="drinks"
-              // id="check-drinks"
-              label="Drinks Had"
-              checked={drinksHad}
-              onClick={() => setDrinksHad(!drinksHad)}
-            ></CheckItem>
-          </div>
-          {/* Custom tracking */}
-          <div className="mb-4">
-            <h2 className="text-xl bold">
-              <b>Custom Attributes</b>
-            </h2>
-
-            <label
-              htmlFor="name"
-              className="block font-semibold text-gray-700 mb-2"
-            >
-              Attribute Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              className="rounded-lg px-4 py-2 w-full focus:outline-blue-500 focus:outline-2"
-              placeholder="What do you want to track?"
-              value={attributeName}
-              onChange={(e) => setAttributeName(e.currentTarget.value)}
-            />
-          </div>
-          <div className="mb-6 flex center">
-            <div className="flex items-center">
-              <RadioItem
-                name="option"
-                value="number"
-                label="Number"
-                isChecked={attributeType === "number"}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  assignType(e);
-                }}
-              ></RadioItem>
+              <CheckItem
+                label="Drinks Had"
+                checked={drinksHad}
+                onClick={() => setDrinksHad(!drinksHad)}
+              ></CheckItem>
             </div>
-            <div className="flex items-center">
-              <RadioItem
-                name="option"
-                value="boolean"
-                label="True/False"
-                isChecked={attributeType === "boolean"}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  assignType(e);
-                }}
-              ></RadioItem>
+            {/* Custom tracking */}
+            <div className="mb-4">
+              <h2 className="text-xl bold">
+                <b>Custom Attributes</b>
+              </h2>
+
+              <label
+                htmlFor="name"
+                className="block font-semibold text-gray-700 mb-2"
+              >
+                Attribute Name
+              </label>
+              <div className="flex">
+                <input
+                  type="text"
+                  id="name"
+                  className="rounded-lg px-4 w-full focus:outline-blue-500 focus:outline-2"
+                  placeholder="What do you want to track?"
+                  value={attributeName}
+                  onChange={(e) => setAttributeName(e.currentTarget.value)}
+                />
+                <ActionButton
+                  onClick={(e: React.MouseEvent<HTMLElement>) => {
+                    e.preventDefault();
+                    writeAttribute();
+                  }}
+                  buttonText="+"
+                  styleTags="w-12"
+                ></ActionButton>
+              </div>
+            </div>
+
+            <div className="mb-6 flex center">
+              <div className="flex items-center">
+                <RadioItem
+                  name="option"
+                  value="number"
+                  label="Number"
+                  isChecked={attributeType === "number"}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    assignType(e);
+                  }}
+                ></RadioItem>
+              </div>
+              <div className="flex items-center">
+                <RadioItem
+                  name="option"
+                  value="boolean"
+                  label="True/False"
+                  isChecked={attributeType === "boolean"}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    assignType(e);
+                  }}
+                ></RadioItem>
+              </div>
             </div>
           </div>
 
-          <div className="flex justify-center">
-            <ActionButton
-              onClick={(e: React.MouseEvent<HTMLElement>) => {
-                e.preventDefault();
-                writeAttribute();
-              }}
-              buttonText="Add Attribute"
-              styleTags="w-96 mt-4"
-            ></ActionButton>
-          </div>
+          <div className="flex justify-center"></div>
           <div className="flex justify-center">
             <ActionButton
               onClick={(e: React.MouseEvent<HTMLElement>) => {
